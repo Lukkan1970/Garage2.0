e93e4f26a4abf2140361ae6901c95ce17c5c6c57
Lukkan1970 <patrik.luukkonen@live.com>
2017-04-24 10:17:46 +0200

    Add project files.

---
+++ Garaget/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Garaget
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Garaget/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Garaget.MvcApplication" Language="C#" %>
