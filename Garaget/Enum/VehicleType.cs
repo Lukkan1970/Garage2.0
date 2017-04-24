0ccac077aef550967772ee1f6aab2c4973780720
megahz59 <mikael.herz@telia.com>
2017-04-24 11:52:37 +0200

    Created datamodel

---
+++ Garaget/Enum/VehicleType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garaget.Enum
+{
+    public enum VehicleType
+    {
+        Car,
+        MotorCycle,
+        Bus,
+        Moped,
+        Trolley,
+        ShoppingCart
+    }
+}
