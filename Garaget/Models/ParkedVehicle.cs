12f92b0ad4b4247963487db7d35be25353d524c6
megahz59 <mikael.herz@telia.com>
2017-04-25 11:50:14 +0200

    Anpassat felmeddelande

--- Garaget/Models/ParkedVehicle.cs
+++ Garaget/Models/ParkedVehicle.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Garaget.Models
 
@@ -14,21 +15,39 @@ namespace Garaget.Models
         {
             TimeOfCheckIn = DateTime.Now;
         }
+
         public int Id { get; set; }
+
         [DisplayName("Type of Vehicle")]
         public VehicleType VehicleType { get; set; }
+
         [DisplayName("Registration Number")]
+        [RegularExpression(pattern: "^[A-Z]{3}[0-9]{3}", ErrorMessage = "Only swedish registration numbers allowed like: AAA999.")]
+        [Required]
+        [StringLength(6)]
         public string RegNo { get; set; }
+
         [DisplayName("Color")]
+        [Required]
         public string Color { get; set; }
+
         [DisplayName("Make")]
+        [Required]
         public string Make { get; set; }
+
         [DisplayName("Model")]
+        [Required]
         public string Model { get; set; }
+
         [DisplayName("Number of Wheels")]
+        [Range(2, 10)]
+        [Required]
         public int NoWheels { get; set; }
 
+
         [DisplayName("Check In Time")]
+        //[DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd hh:mm}", ApplyFormatInEditMode = false)]
         public DateTime TimeOfCheckIn { get; set; }
     }
 }
