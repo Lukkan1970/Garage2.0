82ddac788d330fa8d3e7d524a8f647a04a015762
megahz59 <mikael.herz@telia.com>
2017-04-25 09:03:06 +0200

    Displays tillagda i ParkedVehiclesController och menu ändrad i _layout

--- Garaget/Models/ParkedVehicle.cs
+++ Garaget/Models/ParkedVehicle.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel;
 
 namespace Garaget.Models
 
@@ -14,13 +15,20 @@ namespace Garaget.Models
             TimeOfCheckIn = DateTime.Now;
         }
         public int Id { get; set; }
-        public  VehicleType VehicleType { get; set; }
+        [DisplayName("Type of Vehicle")]
+        public VehicleType VehicleType { get; set; }
+        [DisplayName("Registration Number")]
         public string RegNo { get; set; }
+        [DisplayName("Color")]
         public string Color { get; set; }
+        [DisplayName("Make")]
         public string Make { get; set; }
+        [DisplayName("Model")]
         public string Model { get; set; }
+        [DisplayName("Number of Wheels")]
         public int NoWheels { get; set; }
 
+        [DisplayName("Check In Time")]
         public DateTime TimeOfCheckIn { get; set; }
     }
 }
