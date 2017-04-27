a6db8d1d61a720d6f21a736c58fdd224634f19b4
Lukkan1970 <patrik.luukkonen@live.com>
2017-04-27 13:38:31 +0200

    Created checkoutticket

---
+++ Garaget/Models/ParkedVehiclesStatistics.cs
@@ -0,0 +1,17 @@
+﻿using Garaget.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garaget.Models
+{
+    public class ParkedVehiclesStatistics
+    {
+        public int AmountVehicles { get; set; }
+        public int AmountWheels { get; set; }
+        public Dictionary<VehicleType, int> AmountVehiclePerType { get; set; }
+        public int AmountMinutes { get; set; }
+
+    }
+}
---
+++ Garaget/ViewModels/CheckOutTicketViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Garaget.Enum;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace Garaget.ViewModels
+{
+    public class CheckOutTicketViewModel
+    {
+
+        public int Id { get; set; }
+
+        [DisplayName("Type of Vehicle")]
+        public VehicleType VehicleType { get; set; }
+
+        [DisplayName("Registration Number")]
+        public string RegNo { get; set; }
+
+        [DisplayName("Check In Time")]
+        public DateTime TimeOfCheckIn { get; set; }
+
+        [DisplayName("Total Cost")]
+        public string TotalCost { get; set; }
+
+        
+    }
+}
