f5457575aae205468a9f5c2409590c6d3c1db772
megahz59 <mikael.herz@telia.com>
2017-05-05 09:15:48 +0200

    Moving TotalAmountOfMinutes logic from view to viewmaster.

---
+++ Garaget/ViewModels/StatisticsViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Garaget.Models;
+using System.ComponentModel;
+using Garaget._Enum;
+
+namespace Garaget.ViewModels
+{
+    public class StatisticsViewModel
+    {
+        public StatisticsViewModel()
+        {
+            int index = 0;
+            foreach (var item in Vehicles)
+            {
+                double amountOfMinutes = ((DateTime.Now - item.TimeOfCheckIn).TotalHours);
+                if (VehicleTypeAmount.ContainsKey(item.GetType().ToString()))
+                {
+                    TotalAmountOfMinutes = TotalAmountOfMinutes + (int)amountOfMinutes;
+                }
+                else
+                {
+                    VehicleTypeAmount.Add(item.GetType().ToString(), index++);
+                }
+                AmountOfWheels += item.NoWheels;
+            }
+        }
+
+        public IEnumerable<ParkedVehicle> Vehicles { get; set; }
+
+        [DisplayName("Type of Vehicle")]
+        public Dictionary<string, int> VehicleTypeAmount { get; set; }
+        [DisplayName("Type of Vehicle")]
+        public int AmountOfWheels { get; set; }
+        [DisplayName("Type of Vehicle")]
+        public int TotalAmountOfMinutes { get; set; }
+
+    }
+}
